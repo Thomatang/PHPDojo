d775ce6ce978266e09ce3429db47d214b57e55de
Thomas Bouchard <thomaswardbouchard@gmail.com>
2019-08-09 15:26:48 +0200

    ADD code and tests for 3 name input

--- whoLikesIt/src/WhoLikesIt.php
+++ whoLikesIt/src/WhoLikesIt.php
@@ -5,12 +5,30 @@ namespace App;
 class WhoLikesIt
 {
 
+    /**
+     * @param $names
+     * @return string
+     */
     function likes($names)
     {
-        if (empty($names)) return "";
+        if (empty($names))
+            return "no one likes this.";
 
-        $nameString = implode($names);
-
-        return $nameString . " likes this.";
+        if (sizeof($names) == 1) {
+            $output = $names[0] . " likes this.";
+        } elseif (sizeof($names) == 2) {
+            $output = $names[0] . " and " . $names[1] . " like this.";
+        } else {
+            $output = $names[0];
+            foreach (array_slice($names, 1) as $name) {
+                if ($name == end($names)) {
+                    $output .= " and " . $name;
+                    break;
+                }
+                $output .= ", " . $name;
+            }
+            $output .= " like this.";
+        }
+        return $output;
     }
 }
--- whoLikesIt/tests/WhoLikesItTest.php
+++ whoLikesIt/tests/WhoLikesItTest.php
@@ -5,11 +5,21 @@ use App\WhoLikesIt;
 class WhoLikesItTest extends PHPUnit\Framework\TestCase {
     function testReturnEmptyInputEmptyArray() {
         $whoLikesIt = new WhoLikesIt();
-        $this->assertEquals("", $whoLikesIt->likes([]));
+        $this->assertEquals("no one likes this.", $whoLikesIt->likes([]));
     }
 
     function testInputOneNameReturnCorrectText(){
         $whoLikesIt = new WhoLikesIt();
         $this->assertEquals("Peter likes this.", $whoLikesIt->likes(['Peter']));
+    }
+
+    function testInputTwoNamesReturnCorrectText(){
+        $whoLikesIt = new WhoLikesIt();
+        $this->assertEquals("Peter and John like this.", $whoLikesIt->likes(['Peter', 'John']));
+    }
+
+    function testInputThreeNamesReturnCorrectText(){
+        $whoLikesIt = new WhoLikesIt();
+        $this->assertEquals("Peter, John and Benedict like this.", $whoLikesIt->likes(['Peter', 'John', 'Benedict']));
     }
 }
